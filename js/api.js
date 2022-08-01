4e7589ebcb7381af60d9ef0914546531ebecad98
alma <107155812+almaguerreroher@users.noreply.github.com>
2022-08-01 17:14:30 -0500

    alma-22

--- js/api.js
+++ js/api.js
@@ -1,9 +1,22 @@
+
+//variables principales
 const url = "https://raw.githubusercontent.com/almaguerreroher/alma_API/master/";
 
-fetch(url + "datos.json").then(fuction(respuesta){
+//encontrar elementos HTML
+const el_lista_actores = document.getElementById("actores-lista");
+
+//encontrar desde nuestra propia base de datos
+
+fetch(url + "datos.json").then(fuction(respuesta) {
     resturnrespuesta.json();
 }).then(fuction(datos){
-    console.log(datos)
+    //pasar por cada actor de la base de datos 
+    for(let: 0 : i <datos.length; i += 1){
+
+    //alimentar la lista de actores
+     el_lista_actores.innerHTML "<button>" + datos [i].nombre + "</button>";
+    } 
+
 }).catch(funtion(error){
     console.log(error)
 });
